47cd987871b2e77fc350f99811c8f7ec10eb8d9d
Kaveri <kaveri6303@gmail.com>
2025-07-14 22:17:14 +0530

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import header_img from '../../assets/frontend_assets/header_img.png';
 const Header = () => {
+  //  console.log("Header Image Path →", header_img);
   return (
-    <div className='header'>
+    <div className='header'
+        style={{ backgroundImage: `url(${header_img})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+      
       <div className='header-contents'>
         <h2>Order your favourite food here</h2>
         <p>Craving satisfied, happiness delivered</p>
@@ -10,7 +14,7 @@ const Header = () => {
 
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header
